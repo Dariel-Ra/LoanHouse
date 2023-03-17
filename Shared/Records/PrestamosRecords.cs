1ea97dd9324e8d84929cf460443d3be409940483
Dariel-Ra <116853873+Dariel-Ra@users.noreply.github.com>
2023-03-17 19:09:56 -0400

    Records y Request de Prestamos

--- Shared/Records/PrestamosRecords.cs
+++ Shared/Records/PrestamosRecords.cs
@@ -1,17 +1,13 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace LoanHouse.Server.Models;
-    
-public class Prestamos
+namespace LoanHouse.Shared.Records;
+public class PrestamosRecords
 {
-    [Key]
     public int Prestamos_id { get; set; }
     public int Fecha_prestamo { get; set; }
     public float Monto { get; set; }
     public int Tasa_interes { get; set; }
     public int Plazo { get; set; }
     public int Cliente_id { get; set; }
-    public virtual Cliente Cliente { get; set; } = null!;
+    public virtual ClienteRecord ClienteRecord { get; set; } = null!;
     public int Usuario_id { get; set; }
-    public virtual Usuario Usuario { get; set; } = null!;
+    public virtual UsuarioRecord UsuarioRecord { get; set; } = null!;
 }
