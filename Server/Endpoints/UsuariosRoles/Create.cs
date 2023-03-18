67e4c4c2f7c0bb20f5f25a64d7bdb27774a0ba1e
Dariel-Ra <116853873+Dariel-Ra@users.noreply.github.com>
2023-03-18 16:07:00 -0400

    Fixed

---
+++ Server/Endpoints/UsuariosRoles/Create.cs
@@ -0,0 +1,13 @@
+using LoanHouse.Shared.Records;
+using LoanHouse.Shared.Requests;
+using LoanHouse.Shared.Wrapper;
+
+namespace LoanHouse.Server.Endpoints.UsuariosRoles;
+
+using Request = UsuarioRolCreateRequest;
+using Respuesta = Result<int>;
+
+public class Create
+{
+    
+}
--- Shared/Records/PagoRecord.cs
+++ Shared/Records/PagoRecord.cs
@@ -5,8 +5,6 @@ public class PagoRecord
     public int Pago_id { get; set; }
     public int Fecha_pago { get; set; }
     public float Monto { get; set; }
-    public int Prestamo_id { get; set; }
     public virtual PrestamoRecord Prestamos { get; set; } = null!;
-    public int Usuario_id { get; set; }
     public virtual UsuarioRecord Usuario { get; set; } = null!;
 }
